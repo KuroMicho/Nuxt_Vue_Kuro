db04b83056a6d2095cf561140d94871eed59588c
KuroMicho <kevin0552009.kr@gmail.com>
2019-04-02 13:52:08 -0500

    Hola

---
+++ pages/productos/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <div class="row mt-3">
+            <div class="col-xs-6">
+                <h4>Listado de Productos</h4>
+                
+            </div>
+            <div class="col-xs-6 ml-5">
+                <b-button variant="primary" href="/productos/nuevo">
+                    Nuevo
+                </b-button>
+            </div>
+        </div>
+
+        <div class="row mt-4">
+            <div class="col-sm-12">
+                <b-table responsive striped hover :fields="fields" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            fields: [ 'Imagen', 'Nombre', 'Precio', 'Cantidad', 'Acciones' ]
+        }
+    }
+}
+</script>
+
---
+++ pages/productos/nuevo.vue
@@ -0,0 +1,32 @@
+<template>
+  <b-form>
+        <h1>Nuevo Producto</h1>
+    <div class="row">
+      <div class="col-sm-12">
+        <b-form-group label="Imagen:" label-for="imagen">
+          <b-form-file id="imagen" placeholder="Load Image" drop-placeholder="Load Image"/>
+        </b-form-group>
+
+        <b-form-group label="Nombre:" label-for="nombre">
+          <b-form-input id="nombre" placeholder="Nombre del producto"/>
+        </b-form-group>
+
+        <b-form-group label="Precio:" label-for="precio">
+          <b-form-input id="precio" type="text" required placeholder="Precio del producto"/>
+        </b-form-group>
+
+        <b-form-group label="Cantidad:" label-for="cantidad">
+          <b-form-input id="cantidad" type="number" required placeholder="Cantidad del producto"/>
+        </b-form-group>
+      </div>
+    </div>
+
+        <b-button-toolbar>
+          <b-button-group right class="mx-2">
+            <b-button href="/productos">Volver</b-button>
+            <b-button variant="primary" type="submit">Guardar</b-button>
+          </b-button-group>
+        </b-button-toolbar>
+   
+  </b-form>
+</template>
